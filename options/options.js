0a0098de51f7ff8f6e38d2bec720cb8ec931093c
Crowdbotics <team@crowdbotics.com>
2021-08-15 22:15:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "noisy_mud_29835Identifier",
+  url: "https://noisy_mud_29835Identifier.botics.co",
+  api: "https://noisy_mud_29835Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
